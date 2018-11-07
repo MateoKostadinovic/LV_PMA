7c0259291c06f30364a7eae8344fa96c77adcf6e
MateoKostadinovic <33121958+MateoKostadinovic@users.noreply.github.com>
2018-11-07 10:56:08 +0100

    LV1_full2

--- app/src/main/java/com/example/mateokosta/myapplication/SummaryActivity.java
+++ app/src/main/java/com/example/mateokosta/myapplication/SummaryActivity.java
@@ -1,19 +1,24 @@
 package com.example.mateokosta.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class SummaryActivity extends AppCompatActivity
 {
-
     private String sIme;
     private TextView oIme;
     private String sPredmet;
     private TextView oPredmet;
+    public Button oBtnKraj;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        oBtnKraj = (Button)findViewById(R.id.btnKraj);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_summary);
 
@@ -26,6 +31,12 @@ public class SummaryActivity extends AppCompatActivity
 
         oPredmet = (TextView)findViewById(R.id.textviewPredmet);
         oPredmet.setText(sPredmet);
+
+        oBtnKraj.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(SummaryActivity.this, MainActivity.class));
+            }
+        });
 
     }
 }
