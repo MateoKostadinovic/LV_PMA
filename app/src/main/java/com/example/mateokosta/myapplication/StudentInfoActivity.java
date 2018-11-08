86b5fd30162281408bfb6c6b6eed30f4992df07e
MateoKostadinovic <33121958+MateoKostadinovic@users.noreply.github.com>
2018-11-08 18:00:27 +0100

    LV2(full)

--- app/src/main/java/com/example/mateokosta/myapplication/StudentInfoActivity.java
+++ app/src/main/java/com/example/mateokosta/myapplication/StudentInfoActivity.java
@@ -13,10 +13,21 @@ public class StudentInfoActivity extends AppCompatActivity
 {
     private Button oBtnUpisi;
     private String sIme;
+    private String sPrezime;
+    private String sDatumRodenja;
+
     private TextView oIme;
     //private TextView oTextviewIme;
     private EditText oInputPredmet;
+    private EditText oInputProfesor;
+    private EditText oInputAkGodina;
+    private EditText oInputBrojSati;
+    private EditText oInputBrojSatiLV;
     private String sPredmet;
+    private String sProfesor;
+    private String sAkGodina;
+    private String sBrojSati;
+    private String sBrojSatiLV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,11 +37,17 @@ public class StudentInfoActivity extends AppCompatActivity
         setContentView(R.layout.activity_student_info);
         oBtnUpisi = (Button)findViewById(R.id.btnUpisi);
         oInputPredmet = (EditText)findViewById(R.id.edittextPredmet);
+        oInputProfesor = (EditText)findViewById(R.id.edittextProfesor);
+        oInputAkGodina = (EditText)findViewById(R.id.edittextAkademskaGodina);
+        oInputBrojSati = (EditText)findViewById(R.id.edittextBrojSati);
+        oInputBrojSatiLV = (EditText)findViewById(R.id.edittextBrojSatiLV);
 
 
 
         final Bundle oExtras = getIntent().getExtras();
         sIme = oExtras.getString("ime");
+        sPrezime = oExtras.getString("prezime");
+        sDatumRodenja = oExtras.getString("datum_rodenja");
         //oIme = (TextView)findViewById(R.id.textviewIme);
         //oIme.setText(sIme);
 
@@ -41,10 +58,21 @@ public class StudentInfoActivity extends AppCompatActivity
                 //startActivity(new Intent(StudentInfoActivity.this, SummaryActivity.class)); ne radi jer moramo proslijediti parametre
 
                 sPredmet = oInputPredmet.getText().toString();
+                sProfesor=oInputProfesor.getText().toString();
+                sAkGodina=oInputAkGodina.getText().toString();
+                sBrojSati=oInputBrojSati.getText().toString();
+                sBrojSatiLV=oInputBrojSatiLV.getText().toString();
 
                 Intent oUpisiPredmet = new Intent(getApplicationContext(), SummaryActivity.class);
                 oUpisiPredmet.putExtra("ime", sIme);
+                oUpisiPredmet.putExtra("prezime", sPrezime);
+                oUpisiPredmet.putExtra("datum_rodenja", sDatumRodenja);
+
                 oUpisiPredmet.putExtra("predmet", sPredmet);
+                oUpisiPredmet.putExtra("profesor", sProfesor);
+                oUpisiPredmet.putExtra("ak_godina", sAkGodina);
+                oUpisiPredmet.putExtra("broj_sati", sBrojSati);
+                oUpisiPredmet.putExtra("broj_sati_lv", sBrojSatiLV);
                 startActivity(oUpisiPredmet);
 
             }
--- app/src/main/java/com/example/mateokosta/myapplication/SummaryActivity.java
+++ app/src/main/java/com/example/mateokosta/myapplication/SummaryActivity.java
@@ -11,8 +11,20 @@ public class SummaryActivity extends AppCompatActivity
 {
     private String sIme;
     private TextView oIme;
+    private String sPrezime;
+    private TextView oPrezime;
+    private String sDatumRodenja;
+    private TextView oDatumRodenja;
     private String sPredmet;
     private TextView oPredmet;
+    private String sProfesor;
+    private TextView oProfesor;
+    private String sAkGodina;
+    private TextView oAkGodina;
+    private String sBrojSati;
+    private TextView oBrojSati;
+    private String sBrojSatiLV;
+    private TextView oBrojSatiLV;
     public Button oBtnKraj;
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -24,13 +36,37 @@ public class SummaryActivity extends AppCompatActivity
 
         final Bundle oExtras = getIntent().getExtras();
         sIme = oExtras.getString("ime");
+        sPrezime = oExtras.getString("prezime");
+        sDatumRodenja = oExtras.getString("datum_rodenja");
         sPredmet = oExtras.getString("predmet");
+        sProfesor = oExtras.getString("profesor");
+        sAkGodina = oExtras.getString("ak_godina");
+        sBrojSati = oExtras.getString("broj_sati");
+        sBrojSatiLV = oExtras.getString("broj_sati_lv");
 
         oIme = (TextView)findViewById(R.id.textviewIme);
         oIme.setText(sIme);
 
+        oPrezime = (TextView)findViewById(R.id.textviewPrezime);
+        oPrezime.setText(sPrezime);
+
+        oDatumRodenja = (TextView)findViewById(R.id.textviewDatumRodenja);
+        oDatumRodenja.setText(sDatumRodenja);
+
         oPredmet = (TextView)findViewById(R.id.textviewPredmet);
         oPredmet.setText(sPredmet);
+
+        oProfesor = (TextView)findViewById(R.id.textviewProfesor);
+        oProfesor.setText(sProfesor);
+
+        oAkGodina = (TextView)findViewById(R.id.textviewAkGodina);
+        oAkGodina.setText(sAkGodina);
+
+        oBrojSati = (TextView)findViewById(R.id.textviewBrojSati);
+        oBrojSati.setText(sBrojSati);
+
+        oBrojSatiLV = (TextView)findViewById(R.id.textviewBrojSatiLV);
+        oBrojSatiLV.setText(sBrojSatiLV);
 
         oBtnKraj = (Button)findViewById(R.id.btnKraj);
         oBtnKraj.setOnClickListener(new View.OnClickListener() {
